5f56211f181b96b2948018af7698603a3be72fb6
Maddy <madisonschaaf48@gmail.com>
2022-04-01 13:46:46 -0600

    fixing comments

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-//server will make the localport
-//post/api/notes
-//you wil need api routes with notes.js and index.js
-//you will need a html routes-- this is all the router.get paths
-//if the style doesn't show-- app.use(express.static('public'))
 //route to the data to save the notes
 const { notes } = require('./db/db.json')
 const express = require('express')
@@ -12,16 +7,12 @@ const path = require('path')
 //initiate server
 const app = express()
 
-//these to app.use need to be there everytime you are looking to accept post data
-//parse incoming string or array data-- creates the data and converts it to a key value pairing to access it in the req body
+//parse incoming string or array data-- 
 app.use(express.urlencoded({ extended: true}))
-//parse incoming JSON data-- this is middle wear the use
+//parse incoming JSON data
 app.use(express.json())
+//use extended files for the html 
 app.use(express.static('public'))
-//add the route to show all the saved notes-- this is more likely what you need
-// app.get('/api/notes', (req, res) => {
-//     res.json(notes)
-// })
 
 
 //function to filter to through specific notes
@@ -37,7 +28,7 @@ function filterByQuery(query, notesArray) {
      return filteredResults;
 }
 
-//function the for id-- may not need the filtering but I will need to create ids for each note that is saved
+//function the for id
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0]
     return result
@@ -45,7 +36,6 @@ function findById(id, notesArray) {
 
 
 //accept the POST routes req.body value and the array we want to add the data to
-//this will be executed in the app.post callback function so it will take a new note and add it to the notes array we passed in and then write the new array data to notes.json
 function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note)
@@ -53,8 +43,7 @@ function createNewNote(body, notesArray) {
         path.join(__dirname, './db/db.json'),
         JSON.stringify({ notes: notesArray }, null, 2)
     )
-    //our functions main code will go here
-    //return finished code to post route for response
+
     return note
 }
 
@@ -69,9 +58,7 @@ function validateNote(note) {
 }
 
 
-//add query to show only specified items
-//you likely only need the other request because you won't have a button to filter results
-//if you need to filter something from an array check 11.1.5
+//get the updated notes.json file
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if(req.query) {
@@ -92,11 +79,10 @@ app.get('/api/notes/:id', (req, res) => {
 })
 
 //listening for a post request-- this is the client requesting the server to accept data
-//req.body to package up the data and send it to the server-req.body we access the data and do something with it--write to file
-//test a post without a fetch in the front end you need to use insomina
 app.post('/api/notes', (req, res) => {
     //set id based on what the next index of array will be
     req.body.id = notes.length.toString()
+    
     
     //if any data in req.body is incorrect, send 404 error back
     if (!validateNote(req.body)) {
@@ -104,9 +90,8 @@ app.post('/api/notes', (req, res) => {
     } else {
     //add notes to json file and notes array in this function
     const note = createNewNote(req.body, notes)
-    //req.body is where our incoming content will be
-    //console.log(req.body.id)
-    //this will send the data back to the client
+
+    //send the data back to the client
     res.json(note)
 }
 })
